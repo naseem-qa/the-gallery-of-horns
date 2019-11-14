ccebf369ddfcb0efa7dc54655359e66c2d30771e
naseem <izzatnaseem30@gmail.com>
2019-11-14 12:02:23 +0200

    lab 3 without sort

--- js/app.js
+++ js/app.js
@@ -19,7 +19,7 @@
 //   // clone (copy) the html from inside the photo-template
 //   let template = $('#photo-template').html();
 
-//   // Add the template to the output div
+//   // Add the s to the output div
 //   hornOutput.html(template);
 
 //   // Put the data in
@@ -30,6 +30,7 @@
 //   $('main').append(hornOutput);
 
 // };
+
 
 // function populateSelectBox() {
 //   let seen = {};
@@ -60,140 +61,67 @@
 //   })
 //   .then(() => populateSelectBox());
 
-'use strict'
 
-//Global
-let allHorns = [];
-let keywordList = [];
-let knowCurrentPage;
+
+'use strict';
+
 function Horns(data) {
   this.image_url = data.image_url;
   this.title = data.title;
   this.description = data.description;
   this.keyword = data.keyword;
   this.horns = data.horns;
-  allHorns.push(this);
+  Horns.all.push(this);
 }
-//Rendering Objects
-Horns.prototype.render = function() {
-  $('main').append('<section class = "clone"></section>');
-  let $clone = $('section[class = "clone"]');
-  let hornyTemplate = Handlebars.compile($('#horny-image-template').html());
-  
-  $clone.html(hornyTemplate(this));
-  $clone.removeClass('clone');
-  $clone.attr('class', this.title);
-  $clone.attr('keyword', this.keyword);
+Horns.all = [];
+
+Horns.prototype.render = function () {
+
+  let templateMarkup = $(`#horns-template`).html();
+  let template = Handlebars.compile(templateMarkup);
+  let hornOutput = template(this);
+  $(`#photo-template`).append(hornOutput);
+};
+
+function populateSelectBox() {
+  let seen = {};
+  let select = $('select');
+  select.empty();
+  Horns.all.forEach((horn) => {
+    if (!seen[horn.keyword]) {
+      let option = `<option value="${horn.keyword}">${horn.keyword}</option>`;
+      select.append(option);
+      seen[horn.keyword] = true;
+    }
+  });
+
+  console.log(seen);
 }
-//Adding Keyword Filter
-Horns.prototype.options = function () {
-  if (keywordList.indexOf(this.keyword) === -1) {
-    $('select').append('<option class="keyword"></option>');
-    let $option = $('option[class="keyword"]');
-    $option.find('option').text('value', this.keyword);
-    $option.removeClass('keyword');
-    $option.attr('value', this.keyword);
-    $option.text(this.keyword);
-    keywordList.push(this.keyword);
-  }
-}
-//Read Json File
-function readJson(filepath) {
-  knowCurrentPage = filepath;
-  $.get(filepath, 'json').then(data => {
-    allHorns = []; //clears out the array
-    data.forEach(hornsObj => {
-      new Horns(hornsObj)
-      
-    })
-  }).then(() => {
-    keywordList = []; //clears out list of keywords
-    
-    allHorns.forEach(horn => {
-      horn.render();
-      horn.options();
-      console.log('work!');
-    })
-  })
-}
-//Code modified from Skyler/Nicole
-//Option view handler
-$('#filter-options').on('change', function(){
-  let val = $(this).val();
-  if(val) {
-    $('section').hide();
-    $(`section[keyword="${val}"]`).show();
-  }
+
+$('select').on('change', function () {
+  let selected = $(this).val();
+  $('div').hide();
+  $(`.${selected}`).fadeIn(800);
 });
-//Pagination Button Click Handler - Dry
-$('.pagination-button').on('click', function(event){
-  $('main').empty();
-  $('#filter-options').empty();
-  $('select').append('<option value="default">Filter by Keyword</option>');
-  $(() => readJson(`./data/${event.target.id}.json`));
-});
-//Sort Button Click Handler - Title
-$('#alphabetize').on('click', function(event){
-  $('main').empty();
-  $('#filter-options').empty();
-  $('select').append('<option value="default">Filter by Keyword</option>');
-  $(() => readJsonSortTitle(knowCurrentPage));
-  console.log(`${event.target.id}`);
-});
-//Read Json File for Title Sort
-function readJsonSortTitle(filepath) {
-  $.get(filepath, 'json').then(data => {
-    allHorns= []; //clears out the array
-    data.forEach(hornsObj => {
-      new Horns(hornsObj)
+
+$(`button`).on(`click`, function () {
+  let num = $(this).attr(`id`);
+  readData(num)
+})
+
+function readData(pageNum) {
+  $('div').remove();
+  Horns.all = [];
+  $.get(`../data/page-${pageNum}.json`)
+    .then(data => {
+      data.forEach((thing) => {
+        let horn = new Horns(thing);
+        horn.render();
+      });
     })
-    allHorns.sort(function(a,b){
-      if(a.title<b.title) return -1;
-      if(a.title>b.title) return 1;
-      return 0;
-    })
-  }).then(() => {
-    keywordList = []; //clears out list of keywords
-    allHorns.forEach(horn => {
-      horn.render();
-      horn.options();
-    })
-  })
+    .then(() => populateSelectBox());
 }
-//Sort Button Click Handler - Horns
-$('#horn-sort').on('click', function(event){
-  $('main').empty();
-  $('#filter-options').empty();
-  $('select').append('<option value="default">Filter by Keyword</option>');
-  $(() => readJsonSortHorns(knowCurrentPage));
-  console.log(`${event.target.id}`);
-});
-//Read Json File for Horns Sort
-function readJsonSortHorns(filepath) {
-  $.get(filepath, 'json').then(data => {
-    allHorns= []; //clears out the array
-    data.forEach(hornsObj => {
-      new Horns(hornsObj)
-    })
-    allHorns.sort(function(a,b){
-      if(a.horns<b.horns) return -1;
-      if(a.horns>b.horns) return 1;
-      return 0;
-    })
-  }).then(() => {
-    keywordList = []; //clears out list of keywords
-    allHorns.forEach(horn => {
-      horn.render();
-      horn.options();
-    })
-  })
-}
-//Sort Button Click Handler - Horns
-$('#horn-sort').on('click', function(event){
-  $('main').empty();
-  $('#filter-options').empty();
-  $('select').append('<option value="default">Filter by Keyword</option>');
-  $(() => readJsonSortHorns(knowCurrentPage));
-  console.log(`${event.target.id}`);
-});
-$(() => readJson('./data/page-1.json'))
+
+$(document).ready(function () {
+  readData(1);
+})
